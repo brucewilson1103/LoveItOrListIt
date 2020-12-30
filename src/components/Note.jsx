678cdd85e6c92d388b3f14bd35adc34c243bc601
Bruce Wilson <brucewilson1103@gmail.com>
2020-12-30 17:16:20 -0500

    Initial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Reason title</h1>
+      <h5>Love or List</h5>
+      <p>This is the content and details</p>
+    </div>
+  );
+}
+
+export default Note;
