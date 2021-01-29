aff90f9a1750fd7feb268c6ebd1903ba3d5f1644
Bruce Wilson <brucewilson1103@gmail.com>
2021-01-29 17:51:06 -0500

    Add new house

--- src/note.js
+++ src/note.js
@@ -5,9 +5,19 @@ const notes = [
     url:
       "https://www.zillow.com/homes/18-abbott-ct-flemington-nj_rb/38841823_zpid/",
     content: "Standard 3 bedroom",
-    price: "$403,000",
+    price: "$403,500",
     image:
       "https://maps.googleapis.com/maps/api/streetview?size=576x432&location=40.51055908203125,-74.86292266845703&key=AIzaSyBJsNQO5ZeG-XAbqqWLKwG08fWITSxg33w&&signature=tdEmAVpWHjg6e43lpfUyxCpphfA="
+  },
+  {
+    key: 4,
+    title: "Chelsea's Pick",
+    url:
+      "https://www.zillow.com/homedetails/10-Noreen-Dr-Yardley-PA-19067/9020189_zpid/?mmlb=g,6",
+    image:
+      "https://photos.zillowstatic.com/fp/f82858edf0c295031af91c4c9d400934-uncropped_scaled_within_1536_1152.webp",
+    content: "4 bed 3 bath with Double Garage",
+    price: "$449,000"
   },
   {
     key: 2,
@@ -21,7 +31,7 @@ const notes = [
   },
   {
     key: 3,
-    title: "66 Britton Rd Stockton,",
+    title: "66 Britton Rd Stockton",
     url:
       "https://www.zillow.com/homedetails/66-Britton-Rd-Stockton-NJ-08559/38839655_zpid/",
     image:
@@ -29,6 +39,7 @@ const notes = [
     content: "4 bed 3 bath with Garage",
     price: "$499,000"
   }
+
   // {
   //   key: 4,
   //   title: "Big ideas",
